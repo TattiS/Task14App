26a6e7757557643b9906ab11b81d0b7be36431c0
TattiS <tanyas81@gmail.com>
2018-08-14 14:39:27 +0300

    Добавьте файлы проекта.

---
+++ Task14App/Models/Division.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14App.Classes
+{
+    public class Division
+    {
+        public string Name { get; set; }
+        public IEnumerable<Team> Teams { get; set; }
+    }
+}
---
+++ Task14App/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14App.Classes
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+    }
+}
